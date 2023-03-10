8a5e0826b0a66b2529f298e6eaaa4585456b4283
Ckey230 <Ckey230@gmail.com>
2023-03-09 19:30:10 -0500

    activity 3

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -28,7 +28,13 @@ async function handleSubmit(e) {
 
     const data = await response.json()
 
-    console.log(data)
+    if (response.status === 200) {
+        setCurrentUser(data.user)
+        localStorage.setItem('token', data.token)
+        history.push(`/`)
+    } else {
+        setErrorMessage(data.message)
+    }
 }
 
     return (
