ed09727bc7073324bf94fecd52ae92c6b0d92a59
Ckey230 <Ckey230@gmail.com>
2023-03-11 19:09:58 -0500

    jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -20,5 +20,17 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/profile', async (req,res) => {
+   // try {
+       // let user = await User.fineOne({
+           // where: {
+           //     userId: ?
+           // }
+      //  })
+       // res.json(user)
+   // }catch {
+       // res.json(null)
+   // }
+})
 
 module.exports = router
