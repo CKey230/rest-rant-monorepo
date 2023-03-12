5ef36a5f883c61a96219228499e1dbe83e080e79
Ckey230 <Ckey230@gmail.com>
2023-03-12 15:46:51 -0400

    admin migrations/models/seeder files

---
+++ backend/seeders/20230312194018-add-admin.js
@@ -0,0 +1,22 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('users', [{
+      first_name: 'Chase',
+      last_name: 'Key',
+      email: 'C.Key@example.com',
+      role:'admin',
+      pasword_digest: await bycrypt.hash(process.env.ADMIN_PASSWORDS, 10),
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+  await queryInterface.bulkDelete('users', {
+    email: 'C.Key@example.com'
+  })
+  }
+};
